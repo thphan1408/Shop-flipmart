1fd65033cd5e7971a9b989f101f3a997942628d3
minhtoan1210 <nguyenvuminhtoan1999@gmail.com>
2024-07-08 17:34:34 +0700

    update

--- src/apis/productServices.ts
+++ src/apis/productServices.ts
@@ -1,7 +1,6 @@
 import { TParam } from "../type/productType";
 import axios_auth from "./httpClient";
 
-
 const getAllProduct = (params: TParam) => {
   return axios_auth.get(`/products/search?q=${params.search}&limit=${params.limit}&skip=${params.skip}`);
 };
