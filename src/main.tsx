267499e88bdefa5780801f2a183dd73fb8b3d7b7
minhtoan1210 <nguyenvuminhtoan1999@gmail.com>
2024-07-11 20:45:00 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import ErrorBoundary from "./components/common/ErrorClass.tsx";
 import { HelmetProvider } from "react-helmet-async";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
-import ErrorBoundary from "./components/common/ErrorClass.tsx";
-import { HelmetProvider } from "react-helmet-async";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,7 +13,6 @@ const queryClient = new QueryClient({
       retry: false,
     },
   },
-});
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
